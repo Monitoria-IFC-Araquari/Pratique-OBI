86baef47070edfdc1140422a559897ff81919296
Pauloartur-23 <pauloartur.ifc@gmail.com>
2026-05-15 18:24:24 -0300

    feat: FirstCommit

---
+++ Condicionais/choco_nivel_jr_2009_fase_2/choco.py
@@ -0,0 +1,6 @@
+N, M = map(int, input().split())
+
+if N % (M + 1) == 0:
+    print("Carlos")
+else:
+    print("Paula")
---
+++ Logica Matematica/bolo_nivel_jr_2012_fase_2/bolo.py
@@ -0,0 +1,5 @@
+A, B, C = map(int, input().split())
+
+bolos = min(A // 2, B // 3, C // 5)
+
+print(bolos)
